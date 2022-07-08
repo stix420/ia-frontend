4f544aa6532e14e27ab4faaf8555dc2695606749
stix <admin@stix.finance>
2022-07-08 01:42:21 -0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
+import './stix.module.css'
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
