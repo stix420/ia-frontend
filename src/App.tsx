83563e3b943b07b2963e9eb358077b27efc351e0
stix <admin@stix.finance>
2022-07-09 12:10:19 -0400

    css is hell

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,26 @@
 import { Component, createResource, createSignal } from 'solid-js';
 import stix from './stix.module.css'
-const getData = async () => (await fetch("http://35.233.192.225/")).json();
-const App: Component = () => {
-  const [score, setScore] = createSignal(0);
-  const [long, setLong] = createSignal("TEST 420P");
-  const [short, setShort] = createSignal("TEST 69C");
-  const [direction, setDirection] = createSignal("up");
-  const [target, setTarget] = createSignal(390);
-  const [data, setData] = createSignal("");
-  const [Data] = createResource(data, getData);
-  
-  const updateScore = () => setScore(Math.floor(Math.random() * 100) * (Math.random() > 0.5 ? 1 : -1));
-  const updateLong = () => setLong(Math.random() > 0.5 ? "TEST 420P" : "TEST 69C");
-  const updateShort = () => setShort(Math.random() > 0.5 ? "TEST 420P" : "TEST 69C");
-  const updateDirection = () => setDirection(Math.random() > 0.5 ? "up" : "down");
-  const updateTarget = () => setTarget(direction() === "up" ? 420 : 390);
-  const updateData = () => setData(JSON.stringify(Data(), null, 2));
-  setInterval(() => {
-    updateScore();
-    updateLong();
-    updateShort();
-    updateDirection();
-    updateTarget();
-    updateData();
-  }, 1000);
-  
 
+const getData = async () => (await fetch("http://35.233.192.225/")).json();
+
+
+const App: Component = () => {
+  const [Data, { refetch }] = createResource(getData);
+  
   return (
     <> 
       <div class={stix.navbar}>
         <p class={stix.navheader}>Gamma Assisted Momentum Engine</p>
-        <p class={stix.navbox}>Game Score: <span class={stix.codebox}> { score() } / 200 </span></p>
-        <p class={stix.navbox}>Dealer Short: <span class={stix.codebox}> { short() } </span></p>
-        <p class={stix.navbox}>Dealer Long: <span class={stix.codebox}> { long() } </span></p>
-        <p class={stix.navbox}>Direction: <span class={stix.codebox}> { direction() } </span></p>
-        <p class={stix.navbox}> { score() >= 0 ? "Long gamma is sustaining" : "Short gamma is reversing"
-} momentum { direction()} towards { target() } </p>
-      </div>
-      <div>
-        <span>{ Data.loading && "Loading.." }</span>
-        <pre>{ data() }</pre>
+        <p class={stix.navbox}>Game Score: <span class={stix.codebox}> { Data()?.gameScore } / 200 </span></p>
+        <p class={stix.navbox}>Dealer Short: <span class={stix.codebox}> { Data()?.dealerShort } </span></p>
+        <p class={stix.navbox}>Dealer Long: <span class={stix.codebox}> { Data()?.dealerLong } </span></p>
+        <p class={stix.navbox}>Direction: <span class={stix.codebox}> { Data()?.direction } </span></p>
+        <p class={stix.navbox}> { Data()?.gameScore >= 0 ? "Long gamma is sustaining" : "Short gamma is reversing"
+} momentum { Data()?.direction} towards { Data()?.target } </p>
       </div>
     </>
   );
 };
 
 export default App;
+
